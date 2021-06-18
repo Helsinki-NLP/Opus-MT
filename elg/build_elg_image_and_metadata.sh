4845f22ef55bb701703535c92b8ab11554137801
Sam Hardwick <sam.hardwick@iki.fi>
2021-06-18 10:33:32 +0300

    Reorganise elg Dockerisation

--- elg/build_elg_image_and_metadata.sh
+++ elg/build_elg_image_and_metadata.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ $(basename $(pwd)) != "elg" ]]; then
+    echo "Run this script in the elg/ subdirectory."
+fi
+
 MODEL_VERSION="1.1"
 LANGS=""
 PAIRS=""
@@ -18,6 +22,7 @@ else
     echo "Using existing directory models/"
 fi
 
+echo
 for readme in $(find models/ -name "README.md"); do
     # Parse README files, yaml would be nicer
     # xargs to trim whitespace :)
@@ -26,7 +31,7 @@ for readme in $(find models/ -name "README.md"); do
     echo "In $readme, found source languages ${THIS_SRC_LANGS} and target languages ${THIS_TGT_LANGS}"
 
     for src in $THIS_SRC_LANGS; do
-	$LANGS+="$src "
+	LANGS+="$src "
 	for tgt in $THIS_TGT_LANGS; do
 	    PAIRS+="$src-$tgt "
 	    LANGS+="$tgt "
@@ -34,14 +39,26 @@ for readme in $(find models/ -name "README.md"); do
     done
 done
 
-echo "Ended up with pairs ${PAIRS}"
+echo "Ended up with pairs: ${PAIRS}"
+echo
 
+if ls *.xml 1> /dev/null 2>&1; then
+    read -p "There are stale xml files present. Delete them? [y/n] " yn
+    case $yn in
+	[Yy]* ) rm -f *.xml;;
+	* ) echo "OK, exiting so you can deal with them."; exit;;
+    esac
+fi
 for pair in $PAIRS; do
     src_lang_opt="--source-lang $(echo $pair | cut --delimiter="-" -f1)"
     tgt_lang_opt="--target-lang $(echo $pair | cut --delimiter="-" -f2)"
     # src_region and tgt_region aren't used in Tatoeba, but generate_metadata knows about them
-    python3 generate_metadata.py $src_lang_opt $tgt_lang_opt $src_region_opt $trg_region_opt
+    python3 generate_metadata.py --version $MODEL_VERSION \
+	    $src_lang_opt $tgt_lang_opt $src_region_opt $trg_region_opt
 done
+zip metadata_for_elg.zip *.xml
+rm -f *.xml
+echo "Wrote metadata in metadata_for_elg.zip"
 
 IMAGE_NAME="opus-mt-elg-"$(echo $LANGS | tr ' ' '\n' | sort -u | tr '\n' ' ' | xargs | tr ' ' '-')
 if [[ "$#" -ge 1 ]]; then
@@ -51,7 +68,20 @@ fi
 IMAGE_NAME="helsinkinlp/$IMAGE_NAME:$MODEL_VERSION"
 
 echo "Building with name "$IMAGE_NAME
-   
+
+cd ..
+sudo docker build . -f Dockerfile.base -t opus-mt-base
+
+# This is an annoying hack needed to deal with the fact that elg/ is in the
+# main .dockerignore, and some files needed by the Dockerfile for elg live
+# in the main directory. We copy those files, use them, then delete them.
+
+cp server.py content_processor.py write_configuration.py apply_bpe.py elg/
+cd elg
+python3 write_configuration.py > services.json
 sudo docker build . -t $IMAGE_NAME
+rm server.py content_processor.py write_configuration.py apply_bpe.py \
+   services.json
+
 sudo docker login
 sudo docker push $IMAGE_NAME
